c2d51bb77971f1f9d20672329be741a4bace23bf
lkehl247 <lkehl129@byu.edu>
2025-04-01 17:23:38 -0600

    try

--- backend/Mission11/Mission11/Program.cs
+++ backend/Mission11/Mission11/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors(x =>
-    x.WithOrigins("http://localhost:3000", "https://lemon-sand-0346aa81e.6.azurestaticapps.net")
+    x.WithOrigins("http://localhost:3000", "https://lemon-sand-0346aa81e.6.azurestaticapps.net/")
      .AllowAnyHeader()
      .AllowAnyMethod()
 );
